8c67b1ecfafc8268a9c6dcc1606cb4ed8a64091b
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-08 13:14:20 +0800

    adds Task class and a mock data generator

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by John Crisostomo on 08/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
